ca92ce55ef781115b6d2dee187ca57d862b2b7e7
li_s1 <sichen.li@psi.ch>
2021-10-15 10:27:12 +0200

    parameter

---
+++ AcceLEGOrator/Parameter.py
@@ -0,0 +1,12 @@
+# Author:   Matthias Frey/Sichen Li
+# Date:     28. August 2016
+#
+# This file contains global variables that
+# are specified by the user in the main function
+# and are needed for initializing the accelerator
+# elements.
+
+# need to be specified for doing a simulation
+global gamma_0  # Lorentz factor
+global mass     # particle rest mass in MeV/c^2
+global charge   # particle charge
--- AcceLEGOrator/__init__.py
+++ AcceLEGOrator/__init__.py
@@ -2,6 +2,7 @@ from AcceLEGOrator.Bunch import *
 from AcceLEGOrator.Distribution import *
 from AcceLEGOrator.Map import *
 from AcceLEGOrator.MADXParser import *
+from AcceLEGOrator.Parameter import *
 from AcceLEGOrator.Particle import *
 from AcceLEGOrator.Physics import *
 from AcceLEGOrator.Constants import *
